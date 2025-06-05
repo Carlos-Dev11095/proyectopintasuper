d1cbb7b9d17775db67f408cfdf5b85839a263feb
NeroArt <pacto.polar88@gmail.com>
2025-06-04 22:34:02 -0600

    Remove (bin/css) Se removio linea de archivo inexistente vendor.min.css

--- bin/css.php
+++ bin/css.php
@@ -2,7 +2,7 @@
         href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
 
-    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/<?php echo $ROOT_PATH; ?>/assets/css/vendor/vendor.min.css">
+  
     <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/bootstrap/css/bootstrap.min.css" />
     <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/animate/animate.min.css" />
     <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/animate/custom-animate.css" />
