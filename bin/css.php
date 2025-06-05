a8d18f5caf3fd1046c81f81a6a84988a857b3a5e
NeroArt <pacto.polar88@gmail.com>
2025-06-04 22:06:52 -0600

    refactor (bin /css y js) Se cambiaron por rutas absolutas

--- bin/css.php
+++ bin/css.php
@@ -2,35 +2,35 @@
         href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
 
-
-    <link rel="stylesheet" href="assets/vendors/bootstrap/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="assets/vendors/animate/animate.min.css" />
-    <link rel="stylesheet" href="assets/vendors/animate/custom-animate.css" />
-    <link rel="stylesheet" href="assets/vendors/fontawesome/css/all.min.css" />
-    <link rel="stylesheet" href="assets/vendors/jarallax/jarallax.css" />
-    <link rel="stylesheet" href="assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css" />
-    <link rel="stylesheet" href="assets/vendors/nouislider/nouislider.min.css" />
-    <link rel="stylesheet" href="assets/vendors/nouislider/nouislider.pips.css" />
-    <link rel="stylesheet" href="assets/vendors/odometer/odometer.min.css" />
-    <link rel="stylesheet" href="assets/vendors/swiper/swiper.min.css" />
-    <link rel="stylesheet" href="assets/vendors/ambed-icons/style.css">
-    <link rel="stylesheet" href="assets/vendors/tiny-slider/tiny-slider.min.css" />
-    <link rel="stylesheet" href="assets/vendors/reey-font/stylesheet.css" />
-    <link rel="stylesheet" href="assets/vendors/owl-carousel/owl.carousel.min.css" />
-    <link rel="stylesheet" href="assets/vendors/owl-carousel/owl.theme.default.min.css" />
-    <link rel="stylesheet" href="assets/vendors/bxslider/jquery.bxslider.css" />
-    <link rel="stylesheet" href="assets/vendors/bootstrap-select/css/bootstrap-select.min.css" />
-    <link rel="stylesheet" href="assets/vendors/vegas/vegas.min.css" />
-    <link rel="stylesheet" href="assets/vendors/jquery-ui/jquery-ui.css" />
-    <link rel="stylesheet" href="assets/vendors/timepicker/timePicker.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/<?php echo $ROOT_PATH; ?>/assets/css/vendor/vendor.min.css">
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/animate/animate.min.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/animate/custom-animate.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/fontawesome/css/all.min.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/jarallax/jarallax.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/nouislider/nouislider.min.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/nouislider/nouislider.pips.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/odometer/odometer.min.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/swiper/swiper.min.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/ambed-icons/style.css">
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/tiny-slider/tiny-slider.min.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/reey-font/stylesheet.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/owl-carousel/owl.carousel.min.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/owl-carousel/owl.theme.default.min.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/bxslider/jquery.bxslider.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/bootstrap-select/css/bootstrap-select.min.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/vegas/vegas.min.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/jquery-ui/jquery-ui.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/timepicker/timePicker.css" />
 
     <!-- template styles -->
-    <link rel="stylesheet" href="assets/css/ambed.css" />
-    <link rel="stylesheet" href="assets/css/ambed-responsive.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/css/ambed.css" />
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/css/ambed-responsive.css" />
 
     <!-- modes css -->
-    <link rel="stylesheet" id="jssMode" href="assets/css/ambed-light.css">
+    <link rel="stylesheet" id="jssMode" href="<?php echo $ROOT_PATH; ?>/assets/css/ambed-light.css">
 
 
     <!-- toolbar css -->
-    <link rel="stylesheet" href="assets/vendors/toolbar/css/toolbar.css">
+    <link rel="stylesheet" href="<?php echo $ROOT_PATH; ?>/assets/vendors/toolbar/css/toolbar.css">
