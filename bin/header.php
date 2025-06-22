fc428b9bc4cc8327548b67ca46bf7a4e393a5f6c
Carlos-Dev11095 <carlosroyiga@gmail.com>
2025-06-21 21:12:08 -0600

    header fixed

--- bin/header.php
+++ bin/header.php
@@ -1,63 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Website Title</title>
-    <link rel="icon" type="image/x-icon" href="/ps.ico">
-    </head>
-<body>
-    <header class="main-header clearfix">
-        <div class="main-header__top">
-            <div class="container">
-<<<<<<< HEAD
-                <div class="main-header__top-inner clearfix">
-                    <div class="main-header__logo">
-                        <a href="index.html">
-                            <img src="<?php echo $ROOT_PATH; ?>/assets/images/resources/logo-1.png" alt="" class="dark-logo">
-                            <img src="<?php echo $ROOT_PATH; ?>/assets/images/resources/logo-2.png" alt="" class="light-logo">
-                        </a>
+<header class="main-header clearfix">
+    <div class="main-header__top">
+        <div class="container">
+            <div class="main-header__top-inner clearfix">
+                <div class="main-header__logo">
+                    <a href="index.html">
+                        <img src="<?php echo $ROOT_PATH; ?>/assets/images/resources/logo-1.png" alt="" class="dark-logo">
+                        <img src="<?php echo $ROOT_PATH; ?>/assets/images/resources/logo-2.png" alt="" class="light-logo">
+                    </a>
+                </div>
+                <div class="main-header__top-right">
+                    <div class="main-header__top-right-content">
+                        <div class="main-header__top-address-box">
+                            <ul class="list-unstyled main-header__top-address">
+                                <li>
+                                    <div class="icon">
+                                        <span class="icon-phone-call"></span>
+                                    </div>
+                                    <div class="content">
+                                        <p>Llame en cualquier momento</p>
+                                        <h5><a href="tel:980009630">+ 98 (000) - 9630</a></h5>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <span class="icon-message"></span>
+                                    </div>
+                                    <div class="content">
+                                        <p>Envíe un correo</p>
+                                        <h5><a href="mailto:ambed@company.com">ambed@company.com</a></h5>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon icon--location">
+                                        <span class="icon-location"></span>
+                                    </div>
+                                    <div class="content">
+                                        <p>380 St Kilda Road</p>
+                                        <h5>Melbourne, Australia</h5>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="main-header__top-right-social">
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-facebook"></i></a>
+                            <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
                     </div>
-                    <div class="main-header__top-right">
-                        <div class="main-header__top-right-content">
-                            <div class="main-header__top-address-box">
-                                <ul class="list-unstyled main-header__top-address">
-                                    <li>
-                                        <div class="icon">
-                                            <span class="icon-phone-call"></span>
-                                        </div>
-                                        <div class="content">
-                                            <p>Llame en cualquier momento</p>
-                                            <h5><a href="tel:980009630">+ 98 (000) - 9630</a></h5>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="icon">
-                                            <span class="icon-message"></span>
-                                        </div>
-                                        <div class="content">
-                                            <p>Envíe un correo</p>
-                                            <h5><a href="mailto:ambed@company.com">ambed@company.com</a></h5>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="icon icon--location">
-                                            <span class="icon-location"></span>
-                                        </div>
-                                        <div class="content">
-                                            <p>380 St Kilda Road</p>
-                                            <h5>Melbourne, Australia</h5>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="main-header__top-right-social">
-                                <a href="#"><i class="fab fa-twitter"></i></a>
-                                <a href="#"><i class="fab fa-facebook"></i></a>
-                                <a href="#"><i class="fab fa-pinterest-p"></i></a>
-                                <a href="#"><i class="fab fa-instagram"></i></a>
-                            </div>
-=======
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav class="main-menu clearfix">
+        <div class="main-menu__wrapper clearfix">
+            <div class="container">
                 <div class="main-menu__wrapper-inner clearfix">
                     <div class="main-menu__left">
                         <div class="main-menu__main-menu-box">
@@ -70,10 +67,10 @@
                                     <a href="<?php echo $ROOT_PATH; ?>/quienes_somos/">Nosotros</a>
                                 </li>
                                                                 <li>
-                                    <a href="<?php echo $ROOT_PATH; ?>/decorador_virtual/">Simulador virtual</a>
+                                    <a href="<?php echo $ROOT_PATH; ?>/decorador_virtual/">Simulador Virtual</a>
                                 </li>
                                 <li>
-                                    <a href="<?php echo $ROOT_PATH; ?>/calcular_m2/">Calcular material</a>
+                                    <a href="<?php echo $ROOT_PATH; ?>/calcular_m2/">Calcular Materal</a>
                                 </li>
                                 <li>
                                     <a href="<?php echo $ROOT_PATH; ?>/galeria/">Galeria</a>
@@ -136,99 +133,10 @@
                                 <li><a href="contact.html">Contacto</a>
                                 </li>
                             </ul>
->>>>>>> 56fcd9122a2c98d4b68aceae7a347b02458f20a2
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <nav class="main-menu clearfix">
-            <div class="main-menu__wrapper clearfix">
-                <div class="container">
-                    <div class="main-menu__wrapper-inner clearfix">
-                        <div class="main-menu__left">
-                            <div class="main-menu__main-menu-box">
-                                <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
-                                <ul class="main-menu__list">
-                                    <li class="dropdown current megamenu">
-                                        <a href="<?php echo $ROOT_PATH; ?>/">Inicio</a>
-                                    </li>
-                                    <li>
-                                        <a href="<?php echo $ROOT_PATH; ?>/quienes_somos/">Nosotros</a>
-                                    </li>
-                                    <li>
-                                        <a href="<?php echo $ROOT_PATH; ?>/decorador_virtual/">Decorador</a>
-                                    </li>
-                                    <li>
-                                        <a href="<?php echo $ROOT_PATH; ?>/calcular_m2/">Calcular m²</a>
-                                    </li>
-                                    <li>
-                                        <a href="<?php echo $ROOT_PATH; ?>/galeria/">Galeria</a>
-                                    </li>
-                                    <li class="dropdown">
-                                        <a href="#">+ Páginas</a>
-                                        <ul>
-                                            <li><a href="team.html">Equipo</a></li>
-                                            <li><a href="team-carousel.html">Carrusel de Equipo</a></li>
-                                            <li><a href="team-details.html">Detalles del Equipo</a></li>
-                                            <li><a href="testimonials.html">Testimonios</a></li>
-                                            <li><a href="testimonials-carousel.html">Carrusel de Testimonios</a></li>
-                                            <li><a href="gallery.html">Galería</a></li>
-                                            <li><a href="gallery-carousel.html">Carrusel de Galería</a></li>
-                                            <li><a href="faq.html">Preguntas Frecuentes</a></li>
-                                            <li><a href="404.html">Error 404</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="dropdown">
-                                        <a href="projects.html">Proyectos</a>
-                                        <ul>
-                                            <li><a href="projects.html">Nuestros Proyectos</a></li>
-                                            <li><a href="projects-carousel.html">Carrusel de Proyectos</a></li>
-                                            <li><a href="project-details.html">Detalles del Proyecto</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="dropdown">
-                                        <a href="services.html">Servicios</a>
-                                        <ul>
-                                            <li><a href="services.html">Servicios</a></li>
-                                            <li><a href="services-carousel.html">Carrusel de Servicios</a></li>
-                                            <li><a href="room-wallpapers.html">Papeles Pintados</a></li>
-                                            <li><a href="wall-sheets.html">Planchas de Pared</a></li>
-                                            <li><a href="wall-painting.html">Pintura de Pared</a></li>
-                                            <li><a href="interior-designing.html">Diseño de Interiores</a></li>
-                                            <li><a href="pvc-panels.html">Paneles de PVC</a></li>
-                                            <li><a href="outdoor-design.html">Diseño Exterior</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="dropdown">
-                                        <a href="#">Tienda</a>
-                                        <ul>
-                                            <li><a href="products.html">Productos</a></li>
-                                            <li><a href="product-details.html">Detalles del Producto</a></li>
-                                            <li><a href="cart.html">Carrito</a></li>
-                                            <li><a href="checkout.html">Pago</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="dropdown">
-                                        <a href="blog.html">Blog</a>
-                                        <ul>
-                                            <li><a href="blog.html">Blog</a></li>
-                                            <li><a href="blog-carousel.html">Carrusel de Blog</a></li>
-                                            <li><a href="blog-sidebar.html">Barra Lateral del Blog</a></li>
-                                            <li><a href="blog-sidebar-two.html">Barra Lateral del Blog 02</a></li>
-                                            <li><a href="blog-sidebar-left.html">Barra Lateral Izquierda del Blog</a></li>
-                                            <li><a href="blog-details.html">Detalles del Blog</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="contact.html">Contacto</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </header>
-    </body>
-</html>
+    </nav>
+</header>
