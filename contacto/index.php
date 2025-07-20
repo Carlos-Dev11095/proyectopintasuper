4397dcfe2c3871429b9063e268d250c5a2a65f2c
Carlos-Dev11095 <carlosroyiga@gmail.com>
2025-07-19 23:03:02 -0600

    contacto y quienes somos fix banner

--- contacto/index.php
+++ contacto/index.php
@@ -212,12 +212,12 @@
 
         <!--Page Header Start-->
         <section class="page-header">
-            <div class="page-header-bg" style="background-image: url(/proyectopintasuper/assets/images/backgrounds/page-header-bg.jpg)">
+            <div class="page-header-bg" style="background-image: url(<?php echo $ROOT_PATH; ?>/assets/images/backgrounds/page-header-bg.jpg);">
             </div>
             <div class="container">
                 <div class="page-header__inner">
                     <ul class="thm-breadcrumb list-unstyled">
-                        <li><a href="/proyectopintasuper">Inicio</a></li>
+                        <li><a href="<?php echo $ROOT_PATH; ?>">Inicio</a></li>
                         <li><span>/</span></li>
                         <li>Contacto</li>
                     </ul>
