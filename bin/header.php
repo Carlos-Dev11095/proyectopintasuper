04c5ff626e16878e3e03185ddf9b6dae7210100d
NeroArt <pacto.polar88@gmail.com>
2025-07-26 21:17:47 -0600

    chore (header) Cambio de nombre y link, solo a Simulador virtual y Calcular material

--- bin/header.php
+++ bin/header.php
@@ -65,10 +65,10 @@
                                     <a href="<?php echo $ROOT_PATH; ?>/productos/">Productos</a>
                                 </li>
                                 <li>
-                                    <a href="<?php echo $ROOT_PATH; ?>/decorador_virtual/">Simulador Virtual</a>
+                                    <a href="<?php echo $ROOT_PATH; ?>/simulador_virtual/">Simulador virtual</a>
                                 </li>
                                 <li>
-                                    <a href="<?php echo $ROOT_PATH; ?>/calcular_m2/">Calcular Material</a>
+                                    <a href="<?php echo $ROOT_PATH; ?>/calcular_material/">Calcular material</a>
                                 </li>
                                 <li>
                                     <a href="<?php echo $ROOT_PATH; ?>/quienes_somos/">Nosotros</a>
